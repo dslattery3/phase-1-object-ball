4c018326efe302b7623c855d1ef94bc3a400bb5a
Dalton <slattery.da@gmail.com>
2022-01-13 09:12:58 -0600

    added away team stats

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -20,6 +20,5 @@
             "Ben Gordon": {number: '8',shoe: '15', points: '33', rebounds: '3', assists: '2', steals: '1', blocks: '1', slamDunks: '0'},
             "Brendan Haywood": {number: '33', shoe: '15', points: '6', rebounds: '12', assists: '12', steals: '22', blocks: '5', slamDunks: '12'}
         }
-
     } 
   }
